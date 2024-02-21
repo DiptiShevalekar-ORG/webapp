6e6ee6a014105c65f9bed7e3be6e4a6adeb4a86d
Dipti Shevalekar <shevalekar.d@northeastern.edu>
2024-02-21 16:10:22 -0500

    trial 1

--- Dipti_Shevalekar_002245703_01/packer/unzipAndSystemd.sh
+++ Dipti_Shevalekar_002245703_01/packer/unzipAndSystemd.sh
@@ -1,7 +1,4 @@
 unzip -o ~/webapp.zip -d /tmp/webapp
-
 sudo mv /tmp/webapp/your_service_file.service /etc/systemd/system/
-
 sudo systemctl daemon-reload
-
 sudo systemctl enable your_service_file.service

7ac9b36d5a5232259b520bdb80c8efc276085e9b
Dipti Shevalekar <shevalekar.d@northeastern.edu>
2024-02-20 20:59:18 -0500

    Packer trial

---
+++ Dipti_Shevalekar_002245703_01/packer/installNode.sh
@@ -0,0 +1,6 @@
+sudo dnf module enable nodejs:14 -y
+sudo dnf install nodejs -y
+
+sleep 10
+
+node --version
---
+++ Dipti_Shevalekar_002245703_01/packer/unzipInstall.sh
@@ -0,0 +1,14 @@
+
+
+# sudo dnf install -y expect
+sudo dnf install -y unzip
+
+# sleep 10
+
+# expect << EOF
+
+# expect "Is this ok [y/N]:"
+# send "y\r"
+
+# expect eof
+# EOF

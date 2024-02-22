5a41cb7aa63ec4d840bd33b133d13b72b402bb0b
Dipti Shevalekar <shevalekar.d@northeastern.edu>
2024-02-21 20:01:16 -0500

    trail 50

--- Dipti_Shevalekar_002245703_01/packer/unzipAndSystemd.sh
+++ Dipti_Shevalekar_002245703_01/packer/unzipAndSystemd.sh
@@ -2,6 +2,10 @@
 sudo mkdir /opt/webappUnzipped
 sudo mv /tmp/webapp.zip /opt/
 sudo unzip -o /opt/webapp.zip -d /opt/webappUnzipped
+
+cd /opt/webappUnzipped/Dipti_Shevalekar_002245703_01
+sudo npm install
+
 sudo mv /opt/webappUnzipped/Dipti_Shevalekar_002245703_01/packer/systemdSetup.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable systemdSetup.service

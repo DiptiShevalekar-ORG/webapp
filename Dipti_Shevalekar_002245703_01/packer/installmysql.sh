6e199fc79c7b0a356db553c3e1e203f6744d12a7
Dipti Shevalekar <shevalekar.d@northeastern.edu>
2024-02-24 12:05:36 -0500

    trial

--- Dipti_Shevalekar_002245703_01/packer/installmysql.sh
+++ Dipti_Shevalekar_002245703_01/packer/installmysql.sh
@@ -52,6 +52,6 @@ HOST="localhost"
 DB_USERNAME="root"
 PASSWORD="Cloud@123"
 DATABASE="trial_db"
-PORT=3002
+PORT=3003
 
 mysql --user="$DB_USERNAME" --password="$PASSWORD" --host="$HOST" --port="$PORT" --execute="CREATE DATABASE IF NOT EXISTS $DATABASE;"

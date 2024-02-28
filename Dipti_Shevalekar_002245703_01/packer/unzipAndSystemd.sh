b8097415459059459b042af6498f5d9fe0e54bf0
Dipti Shevalekar <shevalekar.d@northeastern.edu>
2024-02-28 14:47:18 -0500

    trial6

--- Dipti_Shevalekar_002245703_01/packer/unzipAndSystemd.sh
+++ Dipti_Shevalekar_002245703_01/packer/unzipAndSystemd.sh
@@ -11,12 +11,14 @@ sudo unzip -o /opt/webapp.zip -d /opt/webappUnzipped
 
 sudo chown -R csye6225:csye6225 /opt/webappUnzipped/Dipti_Shevalekar_002245703_01/
 
-
 cd /opt/webappUnzipped/Dipti_Shevalekar_002245703_01
 sudo rm -rf node_modules
 sudo npm install
 
 sudo mv /opt/webappUnzipped/Dipti_Shevalekar_002245703_01/packer/systemdSetup.service /etc/systemd/system/
+sudo chown -R csye6225:csye6225 /etc/systemd/system/systemdSetup.service
+sudo chmod 770 /opt/webappUnzipped/Dipti_Shevalekar_002245703_01/
+sudo chmod 770 /etc/systemd/system/systemdSetup.service
 sudo systemctl daemon-reload
 sudo systemctl enable systemdSetup.service
 sudo systemctl start systemdSetup.service

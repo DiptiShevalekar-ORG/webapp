dbce9b276e61b4c902ec755a9cf79a09d56b2585
Dipti Shevalekar <shevalekar.d@northeastern.edu>
2024-03-19 11:41:29 -0400

    trying logging add

--- Dipti_Shevalekar_002245703_01/packer/installOpsAgent.sh
+++ Dipti_Shevalekar_002245703_01/packer/installOpsAgent.sh
@@ -3,24 +3,25 @@
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
-
+#Adding the logging part --- dont forget adding logging:
 sudo bash -c 'cat <<EOF > /etc/google-cloud-ops-agent/config.yaml
-receivers:
-  my-app-receiver:
-    type: files
-    include_paths:
-      - /var/log/Webapp.log
-    record_log_file_path: true
-processors:
-  my-app-processor:
-    type: parse_json
-    time_key: time
-    time_format: "%Y-%m-%dT%H:%M:%S.%L%Z"
-service:
-  pipelines:
-    default_pipeline:
-      receivers: [my-app-receiver]
-      processors: [my-app-processor]
+logging: 
+  receivers:
+    my-app-receiver:
+      type: files
+      include_paths:
+        - /var/log/Webapp.log
+      record_log_file_path: true
+  processors:
+    my-app-processor:
+      type: parse_json
+      time_key: time
+      time_format: "%Y-%m-%dT%H:%M:%S.%L%Z"
+  service:
+    pipelines:
+      default_pipeline:
+        receivers: [my-app-receiver]
+        processors: [my-app-processor]
 EOF'
 
 sudo chown -R csye6225:csye6225 /var/log/

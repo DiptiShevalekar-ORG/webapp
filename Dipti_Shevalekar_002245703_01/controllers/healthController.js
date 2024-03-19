948d41d0be9a80879c804e37c0758b186a8a9766
Dipti Shevalekar <shevalekar.d@northeastern.edu>
2024-03-19 17:19:52 -0400

    Moving file from local to vm

--- Dipti_Shevalekar_002245703_01/controllers/healthController.js
+++ Dipti_Shevalekar_002245703_01/controllers/healthController.js
@@ -58,6 +58,30 @@ const sequelize = new Sequelize(
                 await sequelize.authenticate();
 
                 res.status(200).send();
+                    const methodAllowed = async(req,res)=>{
+        //
+        // res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+        // res.setHeader('Pragma', 'no-cache');
+        // res.setHeader('X-Content-Type-Options', 'nosniff');
+
+        if(req.method==="GET"){
+            try{
+                const contentType = req.headers['content-type'];
+                if (contentType) {
+                    return res.status(400).send();
+                 }
+
+            //    console.log(req.params)
+                if(Object.keys(req.query).length>0 || Object.keys(req.body).length>0 || Object.keys(req.params).length>0){
+                    res.status(400).send();
+                    console.log(req.params)
+                    return;
+                }
+       //         console.log(process.env.DB_USERNAME);
+
+                await sequelize.authenticate();
+
+                res.status(200).send();
                 logger.info(`Succefully connected to Database`)
 
             }catch(error){
@@ -65,7 +89,22 @@ const sequelize = new Sequelize(
                 res.status(503).send();
             }
         }else{
+            logger.error(`unsuccessfullllll connected to Database`)
             res.status(405).send();
+           
+
+        }
+    };
+
+
+            }catch(error){
+
+                res.status(503).send();
+            }
+        }else{
+           
+            res.status(405).send();
+            logger.error(`unsuccessfullllll connected to Database`)
         }
     };
 

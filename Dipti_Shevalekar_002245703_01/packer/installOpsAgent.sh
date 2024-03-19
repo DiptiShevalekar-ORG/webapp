e2138895a47353094aca29120ede6500d9bd71a9
Dipti Shevalekar <shevalekar.d@northeastern.edu>
2024-03-19 10:28:18 -0400

    changing owners

--- Dipti_Shevalekar_002245703_01/packer/installOpsAgent.sh
+++ Dipti_Shevalekar_002245703_01/packer/installOpsAgent.sh
@@ -4,32 +4,27 @@ curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
 
-# sudo bash -c 'cat <<EOF > /etc/google-cloud-ops-agent/config.yaml
+sudo bash -c 'vi <<EOF > /etc/google-cloud-ops-agent/config.yaml
+receivers:
+  my-app-receiver:
+    type: files
+    include_paths:
+      - /var/log/Webapp.log
+    record_log_file_path: true
+processors:
+  my-app-processor:
+    type: parse_json
+    time_key: time
+    time_format: "%Y-%m-%dT%H:%M:%S.%L%Z"
+service:
+  pipelines:
+    default_pipeline:
+      receivers: [my-app-receiver]
+      processors: [my-app-processor]
+EOF'
 
-# receivers:
-#   my-app-receiver:
-#     type: files
-#     include_paths:
-#       - /var/log/Webapp.log
-#     record_log_file_path: true
-
-# processors:
-#   my-app-processor:
-#     type: parse_json
-#     time_key: time
-#     time_format: "%Y-%m-%dT%H:%M:%S.%L%Z"
-
-# service:
-#   pipelines:
-#     default_pipeline:
-#       receivers: [my-app-receiver]
-#       processors: [my-app-processor]
-
-
-# EOF'
-
-# sudo chown -R csye6225:csye6225 /var/log/webapp.log
-# sudo chmod -R 775 /etc/google-cloud-ops-agent/config.yaml
+sudo chown -R csye6225:csye6225 /var/log/
+sudo chmod -R 775 /etc/google-cloud-ops-agent/config.yaml
 
 #sudo vi /etc/google-cloud-ops-agent/config.yaml
-#sudo systemctl restart google-cloud-ops-agent
+sudo systemctl restart google-cloud-ops-agent

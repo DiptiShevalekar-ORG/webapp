d65720559f15def9386a513b24b754bc2ed40613
Dipti Shevalekar <shevalekar.d@northeastern.edu>
2024-03-19 11:38:54 -0400

    adding metrics

--- Dipti_Shevalekar_002245703_01/packer/installOpsAgent.sh
+++ Dipti_Shevalekar_002245703_01/packer/installOpsAgent.sh
@@ -3,27 +3,28 @@
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
-
+sudo chown -R csye6225:csye6225 /var/log/
 sudo bash -c 'vi <<EOF > /etc/google-cloud-ops-agent/config.yaml
-receivers:
-  my-app-receiver:
-    type: files
-    include_paths:
-      - /var/log/Webapp.log
-    record_log_file_path: true
-processors:
-  my-app-processor:
-    type: parse_json
-    time_key: time
-    time_format: "%Y-%m-%dT%H:%M:%S.%L%Z"
-service:
-  pipelines:
-    default_pipeline:
-      receivers: [my-app-receiver]
-      processors: [my-app-processor]
+logging:  
+  receivers:
+    my-app-receiver:
+      type: files
+      include_paths:
+        - /var/log/Webapp.log
+      record_log_file_path: true
+  processors:
+    my-app-processor:
+      type: parse_json
+      time_key: time
+      time_format: "%Y-%m-%dT%H:%M:%S.%L%Z"
+  service:
+    pipelines:
+      default_pipeline:
+        receivers: [my-app-receiver]
+        processors: [my-app-processor]
 EOF'
 
-sudo chown -R csye6225:csye6225 /var/log/
+
 sudo chmod -R 775 /etc/google-cloud-ops-agent/config.yaml
 
 #sudo vi /etc/google-cloud-ops-agent/config.yaml

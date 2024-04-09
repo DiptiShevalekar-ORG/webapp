043ee78893e35c39fa2c2ded24ddc1f99d78d5bb
ShevalekarDipti <shevalekar.d@northeastern.edu>
2024-04-09 16:51:04 -0400

    trying to build the instance template with hardcoded values

--- Dipti_Shevalekar_002245703_01/weappLogs.js
+++ Dipti_Shevalekar_002245703_01/weappLogs.js
@@ -15,7 +15,7 @@ const logger = createLogger({
     ),
     transports: [
         new winston.transports.File({
-            filename: './webapp.log',
+            filename: '/var/log/webapp/webapp.log',
         }),
     ]});
 
